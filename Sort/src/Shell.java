b887794672fe4e860df612a9be911dbd4d9b04aa
tage <lupengkai@yahoo.com>
2015-09-10 12:39:12 +0800

    Shell

---
+++ Sort/src/Shell.java
@@ -0,0 +1,56 @@
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by tage on 15-9-10.
+ */
+public class Shell {
+    public static void sort(Comparable[] a) { //将a[]升序排列
+        int N = a.length;
+        int h = 1;
+        while (h < N / 3) {
+            h = h * 3 + 1;
+        }
+        while (h >= 1) { //将数组变为h有序
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    exch(a, j, j - h);
+                }
+
+            }
+            h = h / 3;
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = args;
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
